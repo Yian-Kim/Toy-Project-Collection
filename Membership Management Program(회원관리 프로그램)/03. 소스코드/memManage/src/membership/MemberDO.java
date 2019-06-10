6db7d39ec2ab6a69792e3e263045a69f36aff1c7
Chanmi Kim <chanmi_dev@naver.com>
2019-06-11 00:12:34 +0900

    ADD FILE [MemberDO.java]
    1. java 파일 추가
    
    - MemberDO.java
    - 소스코드 추가

---
+++ Membership Management Program(회원관리 프로그램)/03. 소스코드/memManage/src/membership/MemberDO.java
@@ -0,0 +1,37 @@
+package membership;
+
+public class MemberDO {
+
+	private String id;//아이디
+	private String name;//이름
+	private String password;//암호
+	//private String phoneNum;//연락처
+	//private String section;//관심분야
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+	
+	
+}
+
+
